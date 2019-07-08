1bd330da8b745f1a924dc12de8b89c34a7fd3ce4
MouadKH9 <mouad.khchich@gmail.com>
2019-07-08 21:42:49 +0100

    Top routes

--- frontend/src/app/pages/dashboard/dashboard.module.ts
+++ frontend/src/app/pages/dashboard/dashboard.module.ts
@@ -1,4 +1,4 @@
-import { GoogleChartsModule } from "angular-google-charts";
+// import { GoogleChartsModule } from "angular-google-charts";
 import { DashboardComponent } from "./dashboard.component";
 import { NgModule } from "@angular/core";
 
@@ -6,7 +6,7 @@ import { ThemeModule } from "../../@theme/theme.module";
 import { Ng2SmartTableModule } from "ng2-smart-table";
 
 @NgModule({
-  imports: [ThemeModule, Ng2SmartTableModule, GoogleChartsModule],
+  imports: [ThemeModule, Ng2SmartTableModule],
   declarations: [DashboardComponent]
 })
 export class DashboardModule {}
